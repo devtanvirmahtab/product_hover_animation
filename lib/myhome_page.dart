da050b915b23e4c77457216cf5901a6a6eb3b77c
devtanvirmahtab <devtanvirtoha@gmail.com>
2023-04-08 15:23:10 +0600

    update

--- lib/myhome_page.dart
+++ lib/myhome_page.dart
@@ -34,152 +34,155 @@ class _HoverCardState extends State<HoverCard> {
           isHovered = false;
         });
       },
-      child: Container(
-        height: 420,
-        width: 320,
-        decoration: BoxDecoration(
-          color: const Color(0xFF242324),
-          borderRadius: BorderRadius.circular(15.0)
-        ),
-        child: Stack(
-          children: [
-            Positioned(
-              left: -5,
-              top: 150,
-              child: Text('Nike',style:GoogleFonts.montserrat(
-                fontWeight: FontWeight.w900,
-                fontSize: 130.0,
-                color: Colors.white10,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15.0),
+        child: Container(
+          height: 420,
+          width: 320,
+          decoration: BoxDecoration(
+            color: const Color(0xFF242324),
+            borderRadius: BorderRadius.circular(15.0)
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                left: -5,
+                top: 150,
+                child: Text('Nike',style:GoogleFonts.montserrat(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 130.0,
+                  color: Colors.white10,
+                ),
+                softWrap: false,
+                ),
               ),
-              softWrap: false,
-              ),
-            ),
-            AnimatedPositioned(
-              duration: const Duration(milliseconds: 475),
-              curve: Curves.easeOut,
-              top:isHovered ?-240 :-80 ,
-              right:isHovered ?-100 :-80 ,
-              child: AnimatedContainer(
+              AnimatedPositioned(
                 duration: const Duration(milliseconds: 475),
                 curve: Curves.easeOut,
-                height: isHovered ? 450 :280,
-                width: isHovered ? 450 :280,
-                decoration: BoxDecoration(
-                  color:  Colors.amber,
-                  borderRadius: BorderRadius.circular(400),
-                ),
-              ),
-            ),
-            AnimatedAlign(
-              duration: const Duration(milliseconds: 375),
-              alignment: isHovered ? const  Alignment(0.0, -0.8) :const  Alignment(0.0, -0.2),
-              child: SizedBox(
-                height: 220,
-                width: 320,
-                child: Image.asset("assets/images/shoes.png"),
-              ),
-            ),
-            AnimatedPositioned(
-              duration: const Duration(milliseconds: 757),
-              curve: Curves.easeOut,
-              bottom: isHovered ? 0.0 : -100.0,
-              child: SizedBox(
-                width: 320,
-                height: 250,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text('Nike Shoes',
-                      style:GoogleFonts.montserrat(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 30.0,
-                      color: Colors.white,
-                    ),
-                      softWrap: false,
-                    ),
-                    const SizedBox(height:5.0),
-                    AnimatedOpacity(
-                      duration:  Duration(milliseconds: isHovered ? 977 : 577 ),
-                      opacity: isHovered ? 1.0 : 0.0,
-                      curve: isHovered ?  Curves.easeInOutBack : Curves.easeOut,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('SIZE : ',
-                            style:GoogleFonts.montserrat(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 16.0,
-                              color: Colors.white,
-                            ),
-                          ),
-                          const SizeButton(text: '7'),
-                          const SizeButton(text: '8'),
-                          const SizeButton(text: '9'),
-                          const SizeButton(text: '10'),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 5),
-                    AnimatedOpacity(
-                      duration:  Duration(milliseconds: isHovered ? 977 : 577 ),
-                      opacity: isHovered ? 1.0 : 0.0,
-                      curve: Curves.easeInOutBack,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Color : ',
-                            style:GoogleFonts.montserrat(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 16.0,
-                              color: Colors.white,
-                            ),
-                          ),
-                          const CircleAvatar(backgroundColor: Colors.red,radius: 9,),
-                          const SizedBox(width:10),
-                          const CircleAvatar(backgroundColor: Colors.green,radius: 9,),
-                          const SizedBox(width:10),
-                          const CircleAvatar(backgroundColor: Colors.amber,radius: 9,),
-                          const SizedBox(width:10),
-                          const CircleAvatar(backgroundColor: Colors.blue,radius: 9,),
-                          const SizedBox(width:10),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            AnimatedPositioned(
-              duration:isHovered ? const Duration(milliseconds: 900) : const Duration(milliseconds: 500),
-              curve: isHovered ?  Curves.easeIn : Curves.easeOut,
-              bottom: isHovered? 20 : -100.0,
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 1000),
-                opacity: isHovered ? 1.0 : 0.0,
-                child: SizedBox(
-                  width: 320.0,
-                  child: Center(
-                    child: TextButton(
-                        onPressed: (){},
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.white,
-                          padding:const  EdgeInsets.all(20.0),
-                          foregroundColor: Colors.black,
-                          shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.0)
-                        )
-                      ),
-                        child:  Text( 'Buy Now', style:GoogleFonts.montserrat(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w600
-                        )),
-                    ),
+                top:isHovered ?-240 :-80 ,
+                right:isHovered ?-100 :-80 ,
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 475),
+                  curve: Curves.easeOut,
+                  height: isHovered ? 450 :280,
+                  width: isHovered ? 450 :280,
+                  decoration: BoxDecoration(
+                    color:  Colors.amber,
+                    borderRadius: BorderRadius.circular(400),
                   ),
                 ),
               ),
-            )
-          ],
+              AnimatedAlign(
+                duration: const Duration(milliseconds: 375),
+                alignment: isHovered ? const  Alignment(0.0, -0.8) :const  Alignment(0.0, -0.2),
+                child: SizedBox(
+                  height: 220,
+                  width: 320,
+                  child: Image.asset("assets/images/shoes.png"),
+                ),
+              ),
+              AnimatedPositioned(
+                duration: const Duration(milliseconds: 757),
+                curve: Curves.easeOut,
+                bottom: isHovered ? 0.0 : -100.0,
+                child: SizedBox(
+                  width: 320,
+                  height: 250,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('Nike Shoes',
+                        style:GoogleFonts.montserrat(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 30.0,
+                        color: Colors.white,
+                      ),
+                        softWrap: false,
+                      ),
+                      const SizedBox(height:5.0),
+                      AnimatedOpacity(
+                        duration:  Duration(milliseconds: isHovered ? 977 : 577 ),
+                        opacity: isHovered ? 1.0 : 0.0,
+                        curve: isHovered ?  Curves.easeInOutBack : Curves.easeOut,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('SIZE : ',
+                              style:GoogleFonts.montserrat(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizeButton(text: '7'),
+                            const SizeButton(text: '8'),
+                            const SizeButton(text: '9'),
+                            const SizeButton(text: '10'),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      AnimatedOpacity(
+                        duration:  Duration(milliseconds: isHovered ? 977 : 577 ),
+                        opacity: isHovered ? 1.0 : 0.0,
+                        curve: Curves.easeInOutBack,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Color : ',
+                              style:GoogleFonts.montserrat(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const CircleAvatar(backgroundColor: Colors.red,radius: 9,),
+                            const SizedBox(width:10),
+                            const CircleAvatar(backgroundColor: Colors.green,radius: 9,),
+                            const SizedBox(width:10),
+                            const CircleAvatar(backgroundColor: Colors.amber,radius: 9,),
+                            const SizedBox(width:10),
+                            const CircleAvatar(backgroundColor: Colors.blue,radius: 9,),
+                            const SizedBox(width:10),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              AnimatedPositioned(
+                duration:isHovered ? const Duration(milliseconds: 900) : const Duration(milliseconds: 500),
+                curve: isHovered ?  Curves.easeIn : Curves.easeOut,
+                bottom: isHovered? 20 : -100.0,
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 1000),
+                  opacity: isHovered ? 1.0 : 0.0,
+                  child: SizedBox(
+                    width: 320.0,
+                    child: Center(
+                      child: TextButton(
+                          onPressed: (){},
+                          style: TextButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            padding:const  EdgeInsets.all(20.0),
+                            foregroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0)
+                          )
+                        ),
+                          child:  Text( 'Buy Now', style:GoogleFonts.montserrat(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600
+                          )),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
